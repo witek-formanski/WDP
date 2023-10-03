e773595284ac09d7eb3423d8f757ad95144bc9ac
witek-formanski <witekformanski@gmail.com>
2023-10-03 19:06:20 +0200

    [#9] intoduce library

---
+++ src/common_library.h
@@ -0,0 +1,16 @@
+struct line_segment
+{
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    line_segment(int a1, int a2, int b1, int b2)
+    {
+        x1 = a1;
+        x2 = a2;
+        y1 = b1;
+        y2 = b2;
+    }
+};
+
