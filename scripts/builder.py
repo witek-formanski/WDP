02d0156169135412971a730c33214ed171450560
wformans <witold.formanski@hexagon.com>
2023-10-03 14:24:46 +0200

    [#2] builder paths still wrong

--- scripts/builder.py
+++ scripts/builder.py
@@ -3,4 +3,4 @@ import sys
 
 file_name = sys.argv[1]
 
-os.system("g++ -o ../src/{}.exe ../src/{}.cpp".format(file_name, file_name))
+os.system("g++ -o src/{}.exe src/{}.cpp".format(file_name, file_name))
