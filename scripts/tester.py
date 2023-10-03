8647a9f4bfd4465fed197f6ac6748f9607e95404
wformans <witold.formanski@hexagon.com>
2023-10-03 14:25:56 +0200

    [#2] adjust working directory

--- scripts/tester.py
+++ scripts/tester.py
@@ -8,12 +8,12 @@ highest_test_number = int(sys.argv[2])
 for test_number in range(highest_test_number+1):
     print(test_number)
 
-    tests_out_path = "../tests/{}{}.out".format(file_name, test_number)
+    tests_out_path = "tests/{}{}.out".format(file_name, test_number)
     with open(tests_out_path, "r") as out:
         expected_output = out.read().strip()
         
-    tests_in_path = "../tests/{}{}.in".format(file_name, test_number)
-    os.system(os.getcwd() + "../src/{}.exe <{} >{}".format(file_name, tests_in_path, "my_output.out"))
+    tests_in_path = "tests/{}{}.in".format(file_name, test_number)
+    os.system(os.getcwd() + "src/{}.exe <{} >{}".format(file_name, tests_in_path, "my_output.out"))
     with open("my_output.out", "r") as out:
         my_output = out.read().strip()
     
