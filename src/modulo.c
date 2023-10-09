6784e6d4046b44debb5b3855f8681bc7253e0245
witek-formanski <witekformanski@gmail.com>
2023-10-09 12:35:14 +0200

    modulo

---
+++ src/modulo.c
@@ -0,0 +1,21 @@
+// zad. 8
+#include<stdio.h>
+
+int does_root_exist(int n)
+{
+    for (int k = 2; k < n/2; k++)
+    {
+        if (k*k % n == 1) return 1;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    printf("%d", does_root_exist(n));
+    return 0;
+}
