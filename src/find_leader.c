31d2c1f206f0bea52b3200789a796934e9861a27
witek-formanski <witekformanski@gmail.com>
2023-10-11 13:24:36 +0200

    add find_leader

---
+++ src/find_leader.c
@@ -0,0 +1,26 @@
+// zad. 5 tablice
+
+// assumption: there is a leader
+
+int find_leader(int n, int t[])
+{
+    int candidate = t[0];
+    int count = 1;
+    for (int i = 1; i < n; ++i)
+    {
+        if (t[i] == candidate)
+        {
+            ++count;
+        }
+        else
+        {
+            --count;
+            if (!count)
+            {
+                candidate = t[i];
+            }
+        }
+    }
+
+    return candidate;
+}
