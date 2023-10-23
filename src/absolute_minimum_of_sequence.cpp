904a11491e715a95c8b3bb04259fbea3f79cdda3
witek-formanski <witekformanski@gmail.com>
2023-10-23 12:37:42 +0200

    abs min of monotonic sequence

---
+++ src/absolute_minimum_of_sequence.cpp
@@ -0,0 +1,40 @@
+// zliczanie zad. 2
+
+#include <vector>
+#include <iostream>
+
+// binary search for zero
+// ciąg jest ściśle monotoniczny (rosnący lub malejący)
+
+int abs_min(std::vector<int> v)
+{
+    int left = 0;
+    int right = v.size() - 1;
+    int middle;
+
+    // check if the sequence is increasing or decreasing
+
+    // we look for the smallest element greater than 0
+    while (left < right)
+    {
+        middle = (left + right) / 2;
+        if (v[middle] > 0)
+        {
+            right = middle;
+        }
+        else // (v[middle] <= 0)
+        {
+            left = middle + 1;
+        }
+    }
+
+    // compare this element and the one that preceeds it
+    // return...
+}
+
+int main()
+{
+    std::vector<int> v{-10, -8, -2, 3, 5, 11, 200};
+
+    std::cout << abs_min(v);
+}
--- src/cyclic_sequence.c
+++ src/cyclic_sequence.c
@@ -6,4 +6,5 @@ int f(int x) // deterministyczna funkcja
 int cyclic_sequence()
 {
     // dwóch biegaczy, jeden biega 2x szybciej
+    // jeden biegacz, w każdym kroku
 }
