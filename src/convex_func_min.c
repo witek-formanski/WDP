6adb4b752914ba1d66e80c25b4868406350d0faa
witek-formanski <witekformanski@gmail.com>
2023-10-23 12:51:38 +0200

    convex min

---
+++ src/convex_func_min.c
@@ -0,0 +1,34 @@
+// zad. 5.2. zliczanie
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int minimum_of_convex_func(int n, int a[])
+{
+    int left = 0;
+    int right = n - 1;
+    int middle;
+
+    // check if the sequence is increasing or decreasing
+
+    // we look for the smallest element greater than 0
+    while (left + 1 < right)
+    {
+        middle = (left + right) / 2;
+        if (a[middle] > a[middle + 1])
+        {
+            left = middle + 1;
+        }
+        else // (v[middle] <= 0)
+        {
+            right = middle;
+        }
+    }
+
+    return min(a[left], a[right]);
+}
+
+int main()
+{
+
+}
