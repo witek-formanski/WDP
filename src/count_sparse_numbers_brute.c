6a5f919e5308e2d8cd75a3cbe644d2bcb30bed07
witek-formanski <witekformanski@gmail.com>
2023-10-31 00:40:27 +0100

    sparse-Fibonacci

--- src/count_sparse_numbers_brute.c
+++ src/count_sparse_numbers_brute.c
@@ -28,5 +28,13 @@ int main() {
 
     printf("%d", count_sparse_numbers_brute(n));
 
+    // surprise - Fibonacci!
+    // for (int i = 0; i < 13; i++) {
+    //     printf("count_sparse(%d) \t- count_sparse(%d) \t= %d\n",
+    //            1 << (i + 1),
+    //            1 << (i),
+    //            count_sparse_numbers_brute(1 << (i + 1)) - count_sparse_numbers_brute(1 << (i)));
+    // }
+
     return 0;
 }
