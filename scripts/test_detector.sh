1d7dd708f19f7129b80977f9c9ccf5553121166a
witek-formanski <witekformanski@gmail.com>
2023-11-04 17:47:30 +0100

    configure pipeline for new Bash script

--- scripts/test_detector.sh
+++ scripts/test_detector.sh
@@ -4,19 +4,24 @@ test_folder=$1
 
 declare -A tests_dict
 
-for file in tests/$test_folder/*; do
+shopt -s globstar
+for file in tests/*/*/$test_folder/*.in; do
+    file_path=$(dirname "${file#tests/}")
+    src_file_path=$(dirname "$file_path")
     file_name=$(basename "$file")
     base_name="${file_name%.*}"
     file_number=$(echo $base_name | grep -o '[0-9]\+$')
     name=$(echo $base_name | sed "s/$file_number//")
-    if [ ${tests_dict[$name]+_} ]; then
-        if (( $file_number > ${tests_dict[$name]} )); then
-            tests_dict[$name]=$file_number
+    full_name="$src_file_path/$name"
+    if [ ${tests_dict[$full_name]+_} ]; then
+        if (( $file_number > ${tests_dict[$full_name]} )); then
+            tests_dict[$full_name]=$file_number
         fi
     else
-        tests_dict[$name]=$file_number
+        tests_dict[$full_name]=$file_number
     fi
 done
+shopt -u globstar
 
 output_env_var=""
 for test_name in "${!tests_dict[@]}"; do
