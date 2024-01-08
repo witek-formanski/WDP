31f23c219cd5bab96c590b24043c5364400c82b3
witek-formanski <witekformanski@gmail.com>
2024-01-08 22:58:20 +0100

    introduce shortest_sequence.cpp

--- src/kol3_22/zad2/shortest_sequence.cpp
+++ src/kol3_22/zad2/shortest_sequence.cpp
@@ -1,5 +1,42 @@
+#include <iostream>
 #include <vector>
 
+int get_bits_count(int n) {
+    int count = 0;
+    while (n != 0) {
+        n /= 2;
+        count++;
+    }
+
+    return count;
+}
+
 std::vector<int> shortest_sequence(int n) {
-    
+    int temp = 1;
+    std::vector<int> sequence{temp};
+    if (n == 1)
+        return sequence;
+
+    int bits_count = get_bits_count(n);
+    for (int i = bits_count - 2; i >= 0; i--) {
+        temp *= 2;
+        sequence.push_back(temp);
+        if ((n & (1 << i)) >> i) {
+            temp += 1;
+            sequence.push_back(temp);
+        }
+    }
+
+    return sequence;
+}
+
+void print_vector(std::vector<int> vect) {
+    for (int x : vect)
+        std::cout << x << " ";
+    std::cout << "\n";
+}
+
+int main() {
+    for (int i = 1; i <= 100; i++)
+        print_vector(shortest_sequence(i));
 }
