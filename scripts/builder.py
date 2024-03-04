c4031ad3df588f30d42aa4b81cee7f0810532c24
witek-formanski <witekformanski@gmail.com>
2024-03-04 19:14:05 +0100

    compile C with strict options and C++ without them

--- scripts/builder.py
+++ scripts/builder.py
@@ -2,13 +2,23 @@
 import sys
 
 full_file_name = sys.argv[1]
+compile_with_options = bool(sys.argv[2])
 file_name = full_file_name.split('.')[0]
 file_extension = full_file_name.split('.')[1]
 
-if (file_extension == 'cpp'):
-    os.system(f'g++ @configs/optionsCpp -o src/{file_name}.exe src/{file_name}.cpp')
-elif (file_extension == 'c'):
-    os.system(f'gcc @configs/options src/{file_name}.c -o src/{file_name}.e')
+if (compile_with_options):
+    if (file_extension == 'cpp'):
+        os.system(f'g++ @configs/optionsCpp -o src/{file_name}.exe src/{file_name}.cpp')
+    elif (file_extension == 'c'):
+        os.system(f'gcc @configs/options src/{file_name}.c -o src/{file_name}.e')
+    else:
+        print('wrong file extension: ', file_extension)
+        sys.exit(1)
 else:
-    print('wrong file extension: ', file_extension)
-    sys.exit(1)
+    if (file_extension == 'cpp'):
+        os.system(f'g++ -o src/{file_name}.exe src/{file_name}.cpp')
+    elif (file_extension == 'c'):
+        os.system(f'gcc src/{file_name}.c -o src/{file_name}.e')
+    else:
+        print('wrong file extension: ', file_extension)
+        sys.exit(1)
